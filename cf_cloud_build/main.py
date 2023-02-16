c2de269dbdb271e721575b7a81ee2ca789f6e43f
Gabriel Luz <gabriel.luz@AC-LPT-00584.local>
2023-02-16 17:23:47 -0300

    test

--- cf_cloud_build/main.py
+++
@@ -1,7 +0,0 @@
-def hello_world(request):
-    request_args = request.args
-    if request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return f'Hello {name}'

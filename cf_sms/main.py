87c4074429256207db38fc4d50f667dd763676aa
Gabriel Luz <gabriel.luz@AC-LPT-00584.local>
2023-03-02 17:49:58 -0300

    Test 3 cf sms

--- cf_sms/main.py
+++ cf_sms/main.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 from google.cloud import secretmanager
 import base64
 
-def hello_sub(event, context):
+def hello_pubsub(event, context):
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     print(pubsub_message)
 
